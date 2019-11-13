5b4a55c6ca34ee80ce0c5cc1ab94a54adc047bcc
Ryan Burrow <ryandburrow@vt.edu>
2019-11-13 08:28:54 -0500

    Reduced by 1 byte

--- xmas.c
+++ xmas.c
@@ -2,6 +2,6 @@ i, j, a;
 main(c, v) int **v; {
     for (a = atoi(v[1]); i < a; i += puts(""))
         for(j = 0; j < a + i;)
-            putchar(++j >= a - i ? 42 : 32);
+            putchar(++j < a - i ? 32 : 42);
     printf("%*s", a, "*");
 }
