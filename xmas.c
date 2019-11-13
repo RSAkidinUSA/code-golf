d7d6fd23fe201c50f4118bdff020cb0b60154562
Ryan Burrow <ryandburrow@vt.edu>
2019-11-13 08:37:25 -0500

    formatting

--- xmas.c
+++ xmas.c
@@ -1,7 +1,7 @@
 i, j, a; 
 main(c, v) int **v; {
     for (a = atoi(v[1]); i < a; i += puts(""))
-        for(j = 0; j < a + i;)
+        for (j = 0; j < a + i;)
             putchar(++j < a - i ? 32 : 42);
     printf("%*s", a, "*");
 }
